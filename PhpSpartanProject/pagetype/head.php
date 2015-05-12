33202fa9a2d889f1ccd2d4b32f92e0d9bf7d92b8
Wolfsister <denis.chenesseau@gmail.com>
2015-05-12 15:43:43 +0200

    Login OK

--- PhpSpartanProject/pagetype/head.php
+++ PhpSpartanProject/pagetype/head.php
@@ -1,25 +1,31 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+    $_SESSION['connected'] = false;
+}
+?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php
-    if($title==''){
+    if ($title == '') {
         echo '<title>OrniCar</title>';
-    }else{
-        echo '<title>'.$title.'</title>';
+    } else {
+        echo '<title>' . $title . '</title>';
     }
     ?>
     <!-- Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
+
     <!-- Bootstrap -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="../code/css/owl.carousel.css">
     <link rel="stylesheet" href="../code/style.css">
@@ -31,4 +37,4 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
