a5e2d8384915bd0abc41d830abeac004820cb7fd
Wolfsister <denis.chenesseau@gmail.com>
2015-05-13 00:11:27 +0200

    Fonction pour tester à l'inscription si pseudo nom prenom et email existent deja

--- PhpSpartanProject/code/fonctions.php
+++ PhpSpartanProject/code/fonctions.php
@@ -27,12 +27,34 @@ function logout(){
     
 }
 
-function isAlreadyRegistered(){
+function isAlreadyRegistered($email,$pseudo,$nom,$prenom){
     $registered=false;
     //Pseudo existe deja => registered=true, echo pseudo deja utilisé
     //Email existe deja
     //Couple Nom/Prenom existe deja
-    
+    $co = connexionBdd();
+    $rechercheMail = "SELECT * FROM user WHERE email='" . $email . "' ";
+    $resultatMail = mysqli_query($co, $rechercheMail);
+    $nbLignes = mysqli_num_rows($resultatMail);
+    if($nbLignes=1){
+        echo 'Cette adresse email est déjà utilisée.';
+        $registered=true;
+    }
+    $recherchePseudo = "SELECT * FROM user WHERE pseudo='" . $pseudo . "' ";
+    $resultatPseudo = mysqli_query($co, $recherchePseudo);
+    $nbLignes = mysqli_num_rows($resultatPseudo);
+    if($nbLignes=1){
+        echo 'Ce pseudo est déjà utilisé.';
+        $registered=true;
+    }
+    $rechercheNomPrenom = "SELECT * FROM user WHERE nom='" . $nom . "' AND prenom='".$prenom."' ";
+    $resultatNomPrenom = mysqli_query($co, $rechercheNomPrenom);
+    $nbLignes = mysqli_num_rows($resultatNomPrenom);
+    if($nbLignes=1){
+        echo 'Votre couple nom/prénom est déjà utilisé.';
+        $registered=true;
+    }
+    return $registered;  
 }
 
 ?>
