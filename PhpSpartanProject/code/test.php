6d041db2b7851af504006358a88b79a7a743d2a6
Wolfsister <denis.chenesseau@gmail.com>
2015-05-14 15:22:25 +0200

    Petits changements, notamment la banniere Ornicar qui pointe sur le bon index (.php)

--- PhpSpartanProject/code/test.php
+++ PhpSpartanProject/code/test.php
@@ -15,15 +15,6 @@ include '../pagetype/hautPage.php';
 
 //    createIDCar();
 
-//$co=  connexionBdd();
-//$requete= "SELECT idUser FROM user WHERE user.email='denis.chenesseau@gmail.com'";
-//$query=mysqli_query($co, $requete);
-//$tabQuery=mysqli_fetch_array($query);
-//echo $tabQuery['idUser'];
-
-
-    $pseudo='Admin';
-    getIdUserByPseudo($pseudo);
 ?>
     
       
