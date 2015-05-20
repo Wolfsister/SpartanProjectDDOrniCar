e2b408d7569a685ac8c00a3292b5351ad344f1ce
Wolfsister <denis.chenesseau@gmail.com>
2015-05-20 09:54:46 +0200

    Dans avis, récupération de tous les trajets auquel l'utilisateur a pris part. A modifier et déplacer dans fonctions.php.  Egalement faire l'affichage. Mettre ligne trajet, cliquer pour afficher avis à donner

--- PhpSpartanProject/code/avis.php
+++ PhpSpartanProject/code/avis.php
@@ -1,24 +1,49 @@
 <?php
-$path='';
-$title='';
+$path = '';
+$title = '';
 include '../pagetype/hautPage.php';
 ?>
 
 <div class="container">
 
     // chercher tous les trajets où la date est antérieure à la date actuelle
-    
+
     <?php
-    $currentTime=date("Y-m-d");
-    $reqTrajets="SELECT * FROM trajet as t, passager as p WHERE anneeMoisJour<'".$currentTime."' AND ((t.idConducteur='".$_SESSION['id']."') OR (t.idTrajet=p.idTrajet AND p.idPassager='".$_SESSION['id']."')) ";
-    echo $reqTrajets;
-    
-    
+        $co = connexionBdd();
+    $tabTousTrajets = array();
+    $currentTime = date("Y-m-d");
+    //Faire aussi test sur l'heure
+    //En conducteur
+    $reqTrajetsConducteur = "SELECT idTrajet FROM trajet as t WHERE anneeMoisJour<'" . $currentTime . "' AND t.idConducteur='" . $_SESSION['id'] . "' ";
+    echo $reqTrajetsConducteur;
+    $sqliCon = mysqli_query($co, $reqTrajetsConducteur);
+    while ($resultCon = mysqli_fetch_array($sqliCon)) {
+        foreach ($resultCon as $key => $value) {
+            if (is_integer($key)) {
+                $tabTousTrajets[] = $value;
+            }
+        }
+    }
+
+
+    //En passager
+    $reqTrajetsPassager = "SELECT p.idTrajet FROM passager as p, trajet as t WHERE anneeMoisJour<'" . $currentTime . "' AND p.idPassager='" . $_SESSION['id'] . "' AND p.idTrajet=t.idTrajet ";
+    echo $reqTrajetsPassager;
+    $sqliPas = mysqli_query($co, $reqTrajetsPassager);
+    while ($resultPas = mysqli_fetch_array($sqliPas)) {
+        foreach ($resultPas as $key => $value) {
+            if (is_integer($key)) {
+                $tabTousTrajets[] = $value;
+            }
+        }
+    }
+
+    print_r($tabTousTrajets);
     ?>
 
 </div>
 
 
-<?php
-include '../pagetype/basPage.php';
-?>
+    <?php
+    include '../pagetype/basPage.php';
+    ?>
--- PhpSpartanProject/code/test.php
+++ PhpSpartanProject/code/test.php
@@ -12,21 +12,45 @@ include '../pagetype/hautPage.php';
     //echo $date;
     $heure = date("H-i");
     //echo $heure;
-
-
 //    $datetime1 = new DateTime('2009-10-11');
 //    $datetime2 = new DateTime('2009-11-13');
 //    $interval = $datetime1->diff($datetime2);   // dt2 - dt1
 //    echo $interval->format('%R%a days');
 //    if( $datetime1<$datetime2){echo 'OK' ;}
-    
-    
-    $currentTime=date("Y-m-d");
-    $reqTrajets="SELECT * FROM trajet as t, passager as p WHERE anneeMoisJour<'".$currentTime."' AND ((t.idConducteur='".$_SESSION['id']."') OR (t.idTrajet=p.idTrajet AND p.idPassager='".$_SESSION['id']."')) ";
-    echo $reqTrajets;
-   
-?>
 
-<?php
-include '../pagetype/basPage.php';
-?>
+
+    $co = connexionBdd();
+    $tabTousTrajets = array();
+    $currentTime = date("Y-m-d");
+    //Faire aussi test sur l'heure
+    //En conducteur
+    $reqTrajetsConducteur = "SELECT idTrajet FROM trajet as t WHERE anneeMoisJour<'" . $currentTime . "' AND t.idConducteur='" . $_SESSION['id'] . "' ";
+    echo $reqTrajetsConducteur;
+    $sqliCon = mysqli_query($co, $reqTrajetsConducteur);
+    while ($resultCon = mysqli_fetch_array($sqliCon)) {
+        foreach ($resultCon as $key => $value) {
+            if (is_integer($key)) {
+                $tabTousTrajets[] = $value;
+            }
+        }
+    }
+
+
+    //En passager
+    $reqTrajetsPassager = "SELECT p.idTrajet FROM passager as p, trajet as t WHERE anneeMoisJour<'" . $currentTime . "' AND p.idPassager='" . $_SESSION['id'] . "' AND p.idTrajet=t.idTrajet ";
+    echo $reqTrajetsPassager;
+    $sqliPas = mysqli_query($co, $reqTrajetsPassager);
+    while ($resultPas = mysqli_fetch_array($sqliPas)) {
+        foreach ($resultPas as $key => $value) {
+            if (is_integer($key)) {
+                $tabTousTrajets[] = $value;
+            }
+        }
+    }
+
+    print_r($tabTousTrajets);
+    ?>
+
+    <?php
+    include '../pagetype/basPage.php';
+    ?>
