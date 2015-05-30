bcaed2df8dd82704fc51085585d032b28affe04b
Wolfsister <denis.chenesseau@gmail.com>
2015-05-30 23:16:05 +0200

    Mise en page de validation recherche trajet améliorée

--- PhpSpartanProject/code/rechercheTrajet_action_action.php
+++ PhpSpartanProject/code/rechercheTrajet_action_action.php
@@ -1,22 +1,32 @@
 <?php
-$path='';
-$title='';
+$path = '';
+$title = '';
 include '../pagetype/hautPage.php';
 ?>
 
 <div class="container">
-    
+
     <?php
-    var_dump($_POST);
-    $idTrajet=$_POST['idTrajet'];
-    $idUser=$_SESSION['id'];
-    $prix=$_POST['prix'];
-    if(participeAuTrajet($idUser, $idTrajet)==false){
+    $idTrajet = $_POST['idTrajet'];
+    $idUser = $_SESSION['id'];
+    $prix = $_POST['prix'];
+    if (participeAuTrajet($idUser, $idTrajet) == false) {
         retirerArgent($idUser, $prix);
         insertIntoPassager($idUser, $idTrajet);
         enleverUnePlaceTrajet($idTrajet);
-    }else{
-        echo 'Vous participez déjà à ce trajet !';
+    } else {
+        ?>
+        <div class="row pplan" id="anim1">
+            <div class="col-md-12 col-xs-12 col-sm-12  pplan">
+                </br>
+                </br>
+                </br>
+                <p>Vous participez déjà à ce trajet !</p>
+                </br>
+                </br>
+            </div>
+        </div>
+        <?php
     }
     ?>
 
